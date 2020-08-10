df3a54b2b1226d1ea4d500f8597aa1386b19290b
VISHWANATH PRATAP SINGH <49968961+vishnupsingh523@users.noreply.github.com>
2020-08-10 10:34:21 +0530

    updated

--- index.js
+++ index.js
@@ -20,12 +20,13 @@ function getResults(name){
         {
             if(name == data.Countries[i].Country)
             {
+                var active = data.Countries[i].TotalConfirmed - data.Countries[i].TotalDeaths - data.Countries[i].TotalRecovered;
                 output += '<ul>'+
                 '<li><b>Country : '+data.Countries[i].Country+ '</li></b><br>'+
                 '<li>New Confirmed : '+data.Countries[i].NewConfirmed+'</li>'+
                 '<li>New Deaths : '+data.Countries[i].NewDeaths+'</li>'+
                 '<li>New Recovered : '+data.Countries[i].NewRecovered+'</li>'+
-                '<li>Active Cases : '+data.Countries[i].TotalConfirmed-data.Countries[i].TotalDeaths-data.Countries[i].TotalRecovered'</li>'+
+                '<li>Active Cases : '+active+'</li>'+
                 '<li>Total Confirmed : '+data.Countries[i].TotalConfirmed+'</li>'+
                 '<li>Total Deaths : '+data.Countries[i].TotalDeaths+'</li>'+
                 '<li>Total Recovered : '+data.Countries[i].TotalRecovered+'</li><br><br>' ;
