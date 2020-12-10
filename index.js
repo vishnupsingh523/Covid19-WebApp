e6b8f85972f69864ad2c873950e3a8fd98951645
VISHWANATH PRATAP SINGH <49968961+vishnupsingh523@users.noreply.github.com>
2020-12-10 10:01:14 +0530

    done

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+// setting the deails animation
+const detail = document.getElementsByClassName('detail')
+// new confirmed caces
+detail[0].style.backgroundImage = "linear-gradient(#FF4136,#FF4136,#FFDC00)"
+// new deaths caces
+detail[1].style.backgroundImage = "linear-gradient(#001f3f,#0074D9,#0074D9)"
+// new recevored caces
+detail[2].style.backgroundImage = "linear-gradient(#2ECC40,#2ECC40,#01FF70)"
+
+// total confirmed caces
+detail[3].style.backgroundImage = "linear-gradient(#FF4136,#FF4136,crimson)"
+// total Deaths caces
+detail[4].style.backgroundImage = "linear-gradient(#001f3f,#001f3f 50%,#0074D9)"
+// Total recevored caces
+detail[5].style.backgroundImage = "linear-gradient(#FFFFFF,#2ECC40 50%,#FFFFFF)"
+
+const newConfirmed = document.getElementById('newConfirmed')
+const newDeaths = document.getElementById('newDeaths')
+const newRecovered = document.getElementById('newRecovered')
+const totalConfirmed = document.getElementById('totalConfirmed')
+const totalDeaths = document.getElementById('totalDeaths')
+const totalRecovered = document.getElementById('totalRecovered')
+
+
 const ptr = document.getElementById('country');
 
 const searchbox = document.querySelector('#country-name');
@@ -20,18 +44,22 @@ function getResults(name){
         {
             if(name == data.Countries[i].Country)
             {
-                var active = data.Countries[i].TotalConfirmed - data.Countries[i].TotalDeaths - data.Countries[i].TotalRecovered;
-                output += '<ul>'+
-                '<li><b>Country : '+data.Countries[i].Country+ '</li></b><br>'+
-                '<li>New Confirmed : '+data.Countries[i].NewConfirmed+'</li>'+
-                '<li>New Deaths : '+data.Countries[i].NewDeaths+'</li>'+
-                '<li>New Recovered : '+data.Countries[i].NewRecovered+'</li>'+
-                '<li>Active Cases : '+active+'</li>'+
-                '<li>Total Confirmed : '+data.Countries[i].TotalConfirmed+'</li>'+
-                '<li>Total Deaths : '+data.Countries[i].TotalDeaths+'</li>'+
-                '<li>Total Recovered : '+data.Countries[i].TotalRecovered+'</li><br><br>' ;
+                newConfirmed.innerText = data.Countries[i].NewConfirmed
+                newDeaths.innerHTML = data.Countries[i].NewDeaths
+                newRecovered.innerHTML = data.Countries[i].NewRecovered
+                totalConfirmed.innerText = data.Countries[i].TotalConfirmed
+                totalDeaths.innerHTML = data.Countries[i].TotalDeaths
+                totalRecovered.innerHTML = data.Countries[i].TotalRecovered
+                // output += '<ul>'+
+                // '<li><b>Country : '+data.Countries[i].Country+ '</li></b><br>'+
+                // '<li>New Confirmed : '+data.Countries[i].NewConfirmed+'</li>'+
+                // '<li>New Deaths : '+data.Countries[i].NewDeaths+'</li>'+
+                // '<li>New Recovered : '+data.Countries[i].NewRecovered+'</li>'+
+                // '<li>Total Confirmed : '+data.Countries[i].TotalConfirmed+'</li>'+
+                // '<li>Total Deaths : '+data.Countries[i].TotalDeaths+'</li>'+
+                // '<li>Total Recovered : '+data.Countries[i].TotalRecovered+'</li><br><br>' ;
 
-                ptr.innerHTML = output;
+                // ptr.innerHTML = output;
             }
         }
     });
